7569baf85d7e71909d21e7023767eaa498e27e8c
SamAddy <addisonsamuel70@yahoo.com>
2023-05-04 21:23:19 +0300

    Add not found page

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from '@mui/material/Link'
+
+const NotFound = () => {
+  return (
+    <div>This page is not available.
+        You may check out the home page.
+        {/* <Link to="/">Home</Link> */}
+    </div>
+  )
+}
+
+export default NotFound
