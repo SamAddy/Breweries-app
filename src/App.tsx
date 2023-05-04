f7aa14f44d2c4edc238e0ba5a39cc8016ae65130
SamAddy <addisonsamuel70@yahoo.com>
2023-05-04 22:15:51 +0300

    Add router for home and not found page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+import CompanyListManageLoading from './components/CompanyList'
+import Home from './pages/Home'
+import CompanyCard from './components/CompanyCard'
+import { Company } from './components/types';
+import HomeManageLoading from './pages/Home';
+import NotFound from './pages/NotFound';
+import CompanyPage from './pages/CompanyPage'
+
+const router = createBrowserRouter ([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        path: "company",
+        element: <CompanyPage />
+      }
+    ]
+  }
+])
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      {/* <HomeManageLoading /> */}
+      {/* <CompanyListManageLoading /> */}
+      <RouterProvider 
+      router={router} 
+      />
+    </div>
   )
 }
 
