64d0466b32ca6ccb640ee9e7626d87e752fbef07
SamAddy <addisonsamuel70@yahoo.com>
2023-05-05 15:33:51 +0300

    Make card clickerable for more details

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,27 +1,13 @@
 import React, { useEffect, useState } from 'react'
-import { Box, Button, Card, CardActions, CardContent, Grid, Typography } from '@mui/material'
+import { Box, Button, Card, CardActionArea, CardActions, CardContent, Grid, Typography } from '@mui/material'
 import axios from 'axios'
 
 import manageLoading from '../components/manageLoading'
-// import './../App.css'
-
-interface Company {
-    id: string,
-    name: string,
-    brewery_type: string,
-    address_1: string,
-    address_2: string | null,
-    city: string,
-    state_province: string
-    postal_code: string,
-    country: string,
-    website_url: string
-}
+import { Company } from '../components/types'
 
 interface Props {
-    data: Company[];
+    data: Company
 }
-
 
 const Home = () => {
     const [companies, setCompanies] = useState<Company[]>([]);
@@ -46,6 +32,7 @@ const Home = () => {
                     <Grid item key={company.id} xs={12} sm={6} md={4}>
                         <Box>
                             <Card variant="outlined">
+                                <CardActionArea>
                                 <CardContent>
                                     <Typography variant="h5" component="h2">
                                         {company.name}
@@ -57,6 +44,7 @@ const Home = () => {
                                         {company.brewery_type}
                                     </Typography>
                                 </CardContent>
+                                </CardActionArea>
                                 <CardActions>
                                     <Button size="small">
                                         <a href={company.website_url} target="_blank" rel="noopener noreferrer">
