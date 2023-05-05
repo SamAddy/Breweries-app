93de27f1275e827b4e48427bc59fabe4c52c4600
SamAddy <addisonsamuel70@yahoo.com>
2023-05-05 15:35:01 +0300

    Add more elements to Company interface

--- src/components/types.tsx
+++ src/components/types.tsx
@@ -4,8 +4,14 @@ export interface Company {
     brewery_type: string,
     address_1: string,
     address_2: string | null,
-    state_province: string
+    city: string,
+    state_province: string,
     postal_code: string,
+    longitude: string,
+    latitude: string
+    phone: string
     country: string,
     website_url: string
+    state: string,
+    street: string
 }
