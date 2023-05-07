9de29184a7dc3a59307b9b93c66e13c51ff27d0b
SamAddy <addisonsamuel70@yahoo.com>
2023-05-07 22:18:12 +0300

    Update router

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,31 @@
 import React from 'react'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
-import CompanyListManageLoading from './components/CompanyList'
+import './App.css'
 import Home from './pages/Home'
-import CompanyCard from './components/CompanyCard'
-import { Company } from './components/types';
-import HomeManageLoading from './pages/Home';
 import NotFound from './pages/NotFound';
 import CompanyPage from './pages/CompanyPage'
-import './App.css'
+import CompanyDetails from './components/CompanyDetails'
 
 const router = createBrowserRouter ([
   {
     path: "/",
     element: <Home />,
     errorElement: <NotFound />,
-    children: [
-      {
-        path: "company",
-        element: <CompanyPage />
-      }
-    ]
+  },
+  {
+    path: "breweries",
+    element: <CompanyPage />
+  },
+  {
+    path: "breweries/:id",
+    element: <CompanyDetails />
   }
 ])
 
 const App = () => {
   return (
     <div>
-      {/* <HomeManageLoading /> */}
-      {/* <CompanyListManageLoading /> */}
       <RouterProvider 
       router={router} 
       />
