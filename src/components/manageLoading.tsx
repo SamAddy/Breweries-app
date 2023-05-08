668b5ad93115c311e799fe55abfb4d5a605412d9
SamAddy <addisonsamuel70@yahoo.com>
2023-05-08 13:57:34 +0300

    Remove extra comments

--- src/components/manageLoading.tsx
+++ src/components/manageLoading.tsx
@@ -1,11 +1,10 @@
 import axios, {AxiosError} from 'axios'
-import { url } from 'inspector'
 import React, { useEffect, useState } from 'react'
 
 const manageLoading = <T extends unknown>(ChildComponent: (props: { data: T }) => JSX.Element, url: string) => {
     return () => {
         const [data, setData] = useState<T | null>(null)
-        const [error, setError] = useState("") //useState<string>("")
+        const [error, setError] = useState<string>("")
         const [loading, setLoading] = useState<boolean>(true)
         useEffect(() => {
             axios
@@ -19,7 +18,7 @@ const manageLoading = <T extends unknown>(ChildComponent: (props: { data: T }) =
                     setError(error.message)
                     setLoading(false)
                 })
-            }, [url])
+            }, [])
         if (loading) {
             return <p>Data is been loaded...</p>
         }
