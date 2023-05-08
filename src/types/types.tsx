33ecbcbe28ceab4cc303153ebcc65e63b1439a08
SamAddy <addisonsamuel70@yahoo.com>
2023-05-08 21:04:12 +0300

    Move types to its folder

---
+++ src/types/types.tsx
@@ -0,0 +1,17 @@
+export interface Company {
+    id: string,
+    name: string,
+    brewery_type: string,
+    address_1: string,
+    address_2: string | null,
+    city: string,
+    state_province: string,
+    postal_code: string,
+    longitude: string,
+    latitude: string
+    phone: string
+    country: string,
+    website_url: string
+    state: string,
+    street: string
+}
