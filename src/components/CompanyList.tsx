8fc6951d711008108d54af3f6715f7a91b254916
SamAddy <addisonsamuel70@yahoo.com>
2023-05-08 21:08:19 +0300

    Add nav to be inheritted by other pages

--- src/components/CompanyList.tsx
+++ src/components/CompanyList.tsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
+import axios, { AxiosError } from 'axios'
+import { Grid, Box, Card, CardActionArea, CardContent, Typography, CardActions, Button, TextField } from '@mui/material'
+import { Link } from 'react-router-dom'
 
 import manageLoading from './manageLoading'
 
@@ -15,14 +18,89 @@ interface Company {
     website_url: string
 }
 
+const useDebounce = <T,>(func: (items: T[], filter: string) => T[], items: T[], delay: number = 1000) => {
+  const [filteredData, setFilteredData] = useState(items)
+  const [filter, setFilter] = useState("")
+  useEffect(() => {
+      const timer = setTimeout(() => {
+          setFilteredData(func(items, filter))
+      }, delay)
+      return () => {
+          clearTimeout(timer)
+      }
+  }, [filter, func, items, delay])
+  const onChangeFilter = (event: React.ChangeEvent<HTMLInputElement>) => {
+      setFilter(event.target.value)
+  }
+  return { onChangeFilter, filter, filteredData }
+}
+
+const filterBreweries = (breweries: Company[], filter: string) => {
+  return breweries.filter(brewery => brewery.name.toLowerCase().includes(filter.toLowerCase()))
+}
+
 const CompanyList = ({ data }: { data: Company[] }) => {
+  const [companies, setCompanies] = useState<Company[]>([])
+    const { onChangeFilter, filter, filteredData } = useDebounce<Company>(filterBreweries, companies)
+    useEffect(() => {
+        axios.get('https://api.openbrewerydb.org/v1/breweries')
+            .then(response => {
+                setCompanies(response.data)
+            })
+            .catch(e => {
+                const error = e as AxiosError 
+                console.log(error.message)
+            })
+    }, [])
+    const memoizedCompanyCards = useMemo(() => {
+      return filteredData.map(company => (
+        <Grid item key={company.id} xs={12} sm={6} md={4}>
+          <Box>
+            <Card variant="outlined">
+              <CardActionArea>
+              <Link to={`/breweries/${company.id}`}>
+                <CardContent className='card'>
+                  <Typography variant="h5" component="h2">
+                    {company.name}
+                  </Typography>
+                  <Typography color="textSecondary">
+                    {company.city}, {company.state_province}
+                  </Typography>
+                  <Typography variant="body2" component="p">
+                    {company.brewery_type}
+                  </Typography>
+                </CardContent>
+                </Link>
+              </CardActionArea>
+              <CardActions>
+                <Button size="small" href={company.website_url} target="_blank" rel="noopener noreferrer">
+                  Visit Brewery Site
+                </Button>
+              </CardActions>
+            </Card>
+          </Box>
+        </Grid>
+      ))
+  }, [filteredData])
   return (
     <div>
-        {data.map((item: Company) => 
-        (<p key ={ item.id }>{ item.name }</p>))}
+      <Typography variant="h4" align="center" className='caption' gutterBottom>
+        Breweries
+        <TextField
+          placeholder="Search by name"
+          variant="outlined"
+          className="search"
+          value={filter}
+          onChange={onChangeFilter}
+        />
+      </Typography>
+      <Grid container spacing={3}>
+        {memoizedCompanyCards}
+      </Grid>
     </div>
   )
 }
 
 const CompanyListManageLoading = manageLoading(CompanyList, "https://api.openbrewerydb.org/v1/breweries")
+
 export default CompanyListManageLoading
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,91 +1,35 @@
-import React, { useEffect, useMemo, useState } from 'react'
-import { Box, Button, Card, CardActionArea, CardActions, CardContent, Grid, TextField, Typography } from '@mui/material'
-import axios, { AxiosError } from 'axios'
-import { Link } from 'react-router-dom'
+import React from 'react'
+import {Typography } from '@mui/material'
+import { Link, Outlet } from 'react-router-dom'
+import GitHub from '@mui/icons-material/GitHub'
 
 import manageLoading from '../components/manageLoading'
-import { Company } from '../components/types'
-
-const useDebounce = <T,>(func: (items: T[], filter: string) => T[], items: T[], delay: number = 1000) => {
-    const [filteredData, setFilteredData] = useState(items)
-    const [filter, setFilter] = useState("")
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            setFilteredData(func(items, filter))
-        }, delay)
-        return () => {
-            clearTimeout(timer)
-        }
-    }, [filter, func, items, delay])
-    const onChangeFilter = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setFilter(event.target.value)
-    }
-    return { onChangeFilter, filter, filteredData }
-}
-
-const filterBreweries = (breweries: Company[], filter: string) => {
-    return breweries.filter(brewery => brewery.name.toLowerCase().includes(filter.toLowerCase()))
-}
 
 const Home = () => {
-    const [companies, setCompanies] = useState<Company[]>([])
-    const { onChangeFilter, filter, filteredData } = useDebounce<Company>(filterBreweries, companies)
-    useEffect(() => {
-        axios.get('https://api.openbrewerydb.org/v1/breweries')
-            .then(response => {
-                setCompanies(response.data)
-            })
-            .catch(e => {
-                const error = e as AxiosError 
-                console.log(error.message)
-            })
-    }, [])
-    const memoizedCompanyCards = useMemo(() => {
-        return filteredData.map(company => (
-          <Grid item key={company.id} xs={12} sm={6} md={4}>
-            <Box>
-              <Card variant="outlined">
-                <CardActionArea>
-                <Link to={`/breweries/${company.id}`}>
-                  <CardContent className='card'>
-                    <Typography variant="h5" component="h2">
-                      {company.name}
-                    </Typography>
-                    <Typography color="textSecondary">
-                      {company.city}, {company.state_province}
-                    </Typography>
-                    <Typography variant="body2" component="p">
-                      {company.brewery_type}
-                    </Typography>
-                  </CardContent>
-                  </Link>
-                </CardActionArea>
-                <CardActions>
-                  <Button size="small" href={company.website_url} target="_blank" rel="noopener noreferrer">
-                    Visit Brewery Site
-                  </Button>
-                </CardActions>
-              </Card>
-            </Box>
-          </Grid>
-        ))
-    }, [filteredData])
-    return (
-        <div>
-            <Typography variant="h4" align="center" className='caption' gutterBottom>
-                Breweries
-                <TextField
-                    placeholder="Search by name"
-                    variant="outlined"
-                    className="search"
-                    value={filter}
-                    onChange={onChangeFilter}
-                />
-            </Typography>
-            <Grid container spacing={3}>
-                {memoizedCompanyCards}
-            </Grid>
+  return (
+    <div>
+      <header>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="breweries">Breweries</Link>
+          <Link to="contact">Contact</Link>
+        </nav>
+      </header>
+      <hr />
+      <Outlet />
+      <hr />
+      <footer>
+        <div className="contact">
+          <a href="https://github.com/SamAddy" target="_blank" rel="noopener noreferrer">
+            <GitHub />
+          </a>
+          <Link to='/'>Home</Link>
+          <Link to='breweries'>Breweries</Link>
+          <Link to='contact'>Contact</Link>
         </div>
+        <Typography className="copyright">&copy; 2023 SamAddy</Typography>
+      </footer>
+    </div>
     )
 }
 
